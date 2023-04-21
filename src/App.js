df231502ffbc4592b17383060cd7b9dbc2872108
erikscvetkovs <eriks.cvetkovs@dynamicyield.com>
2023-04-21 11:49:40 +0300

    header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Header from './routes/header/header.component';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={}>
-        <Route index element={}></Route>
+      <Route path="/" element={<Header/>}>
+        <Route index/>
       </Route>
     </Routes>
   );
---
+++ src/routes/header/header.component.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <div>
+            dd
+        </div>
+    )
+}
+
+export default Header;
+
